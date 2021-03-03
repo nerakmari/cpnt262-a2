0d454af186d939a4bf5c5bf7c60037da13d276eb
Karen Sule <kmsule@gmail.com>
2021-03-02 17:59:38 -0700

    finalized js code, added mediocre styles, and readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+# CPNT262 - Dynamic Image Gallery 
+
+[Github](https://github.com/nerakmari/cpnt262-a2)
+[GH Pages](https://nerakmari.github.io/cpnt262-a2/)
+
+## Context 
+- Taken from CPNT260-a3 assignment, the images are from that gallery. 
+
+## Attributions 
+
+All images are from [unDraw](https://undraw.co/illustrations) | [Copyright 2021 Katerina Limpitsouni](https://undraw.co/license)
+
+The usual honorary mentions: 
+- Kyle, Erica, Ellie, Swapna 
+
+Big helper for this assignment:
+- Vitaly (The GOAT) helped with checking my code and telling me to calm down when I'm panicking
+
+- Erica was a big help because I've been trying to replicate what was done in class by using gallery HTML but I missed and she helped me with setting up the code.
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const steps = [
   id: 1,
   title: 'Step 1',
   description: 'In step 1, you will read our policy and agree with our terms.',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image1.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -17,7 +17,7 @@ const steps = [
   id: 2,
   title: 'Step 2',
   description: 'We want to get to know you more! So, upload any documents or pictures that you may want to share with us to better understand you.',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image2.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -28,7 +28,7 @@ const steps = [
   id: 3,
   title: 'Step 3',
   description: 'Through our advanced technologies, we will find a match for you in every part of the world.', 
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image3.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -39,7 +39,7 @@ const steps = [
   id: 4,
   title: 'Step 4',
   description: 'As we connect you to people all around the world, you will receive a random mail coming from any of those people who matched with you!',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image4.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -50,7 +50,7 @@ const steps = [
   id: 5,
   title: 'Step 5',
   description: 'The next step is all for you! You will read the received mail and rejoice on the wonderful writings they have written for you!',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image5.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -61,7 +61,7 @@ const steps = [
   id: 6,
   title: 'Step 6',
   description: 'While you are out there trying to write out what you want to reply to your beloved match, we will be analyzing the results of your match.',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image6.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -72,7 +72,7 @@ const steps = [
   id: 7,
   title: 'Step 7',
   description: 'Now the fun part! You and your match can schedule a meeting to start your new adventures!',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image7.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -83,7 +83,7 @@ const steps = [
   id: 8,
   title: 'Step 8',
   description: 'With successful meetings, comes successful friendship! You have finally gained a new friend!',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image8.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -94,7 +94,7 @@ const steps = [
   id: 9,
   title: 'Step 9',
   description: 'But it does not have to stop there! We will contact you or will email you a form and review our service. With this, we can improve our system and make current and future clients more satisfied.',
-  width: '500px',
+  width: '400px',
   height: '300px',
   pathURL: './assets/images/image9.png',
   imgURL: 'https://undraw.co/illustrations',
@@ -105,13 +105,18 @@ const steps = [
 
 //Creating a loop for the the arrays 
 
-const gallery = document.querySelector('.gallery');
+// creating a const for gallery id
+const gallery = document.querySelector('#gallery');
 
 let output = '';
 
+// used forEach to create a loop 
 steps.forEach(function (images){
-  output += `<figure class="image-item">
-  <img src="./assets/images/${images.pathURL}.png" alt="${images.title}"/>
+
+  // used gallery HTML to achieve output of images, title, description and width. Did not insert credits as it is all the same for each image and is done through HTML instead
+  output += `<h2>${images.title}</h2>
+  <figure class="image-item">
+  <img src="${images.pathURL}" alt="${images.title}" width="${images.width}"/>
   <figcaption>${images.description}</figcaption>
   </figure>`;
 
